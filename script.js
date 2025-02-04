a690a3ce0e38fe74e24cbdabb889da4e00190a5c
Jenisadevi <163563506+Jenisadevi@users.noreply.github.com>
2025-02-04 22:54:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add scroll event listener to change navbar style
+window.addEventListener('scroll', function () {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
